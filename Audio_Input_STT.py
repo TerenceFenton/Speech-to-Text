96c3985426ff474c077568caa158985b0070aa27
Terence Fenton <moutarde001@gmail.com>
2024-11-10 16:03:33 +1300

    Audio Recorder Debugged

--- Audio_Input_STT.py
+++ Audio_Input_STT.py
@@ -5,29 +5,34 @@ import pyaudio
 import wave
 
 # Audio Parameters
-chunkk = 1024
-formatt = pyaudio.paInt16
-channelss = 1
-ratee = 44100
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
 
 # PyAudio Instance
 instance = pyaudio.PyAudio()
 
-stream = instance.open(format=formatt, channels=channelss, rate=ratee, input=True, frames_per_buffer=chunkk)
+stream = instance.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
 # Recorder Func
-def voice_recorder(chunkk, ratee):
+def voice_recorder(CHUNK, RATE):
     frames = []
     seconds = 3
-    for i in range(0, int(ratee / chunkk * seconds)):
-        data = stream.read(chunkk)
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK, exception_on_overflow = False)
         frames.append(data)
     stream.stop_stream()
     stream.close()
+    instance.terminate()
     return frames
 
 # Save File Func
-def save_file(channelss, formatt, ratee, frames, instance):
+def save_file(FRAMES):
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    instance = pyaudio.PyAudio()
     prompt = input(f'Save File? (Y/N): ')
     real_inputs = ['Y', 'N', 'y', 'n']
 
@@ -37,14 +42,14 @@ def save_file(channelss, formatt, ratee, frames, instance):
     
     if prompt == 'Y' or prompt == 'y':
         wf = wave.open("output.wav", 'wb')
-        wf.setnchannels(channelss)
-        wf.setsampwidth(instance.get_sample_size(formatt))
-        wf.setframerate(ratee)
-        wf.writeframes(b''.join(frames))
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(instance.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(FRAMES))
         wf.close()
 
 # Interactive Func
-def interactive_experience(instance, chunkk, formatt, channelss, ratee):
+def interactive_experience(instance, CHUNK, FORMAT, CHANNELS, RATE):
     prompt = input(f'Start Recording? (Y/N): ')
     real_inputs = ['Y', 'N', 'y', 'n']
 
@@ -54,12 +59,12 @@ def interactive_experience(instance, chunkk, formatt, channelss, ratee):
     
     if prompt == 'Y' or prompt == 'y':
         print('Recording Sample')
-        frames = voice_recorder(chunkk, ratee)
+        frames = voice_recorder(CHUNK, RATE)
         print('Recording Complete')
-        instance.terminate()
-        save_file(instance, frames, formatt, channelss, ratee)
+        save_file(frames)
 
     print('Have a great day')
 
-
+# Main
+interactive_experience(instance, CHUNK, FORMAT, CHANNELS, RATE)
 
