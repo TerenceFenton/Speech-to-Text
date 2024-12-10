cf748ba0c685a9b8d7baf225b4f0a1ca522e112e
Terence Fenton <moutarde001@gmail.com>
2024-12-10 19:31:22 +1300

    Debugged and Finalized, should be ready for use.

--- Main_STT.py
+++ Main_STT.py
@@ -5,32 +5,35 @@
 # Libraries
 import azure.cognitiveservices.speech as azuresystem
 from azure.cognitiveservices.speech import AudioConfig
-from azure.cognitiveservices.speech import ResultReason
 import thread
 from pynput import keyboard
 import time
+import os
 
 # Recognizer Events:
-def recognized_handler(source, event_args):
+def recognized_handler(event_args):
     global final_text
-    print(f'Recognized ({source}): {event_args.result.text}')
+    print(f'Recognized: {event_args.result.text}')
     final_text = event_args.result.text
 
-def recognizing_handler(source, event_args):
-    print(f'Recognizing ({source}): {event_args.result.text}')
+def recognizing_handler(event_args):
+    global final_text
+    print(f'Recognizing: {event_args.result.text}')
 
-def cancelation_handler(source, event_args):
-    print(f'Canceled ({source}): {event_args.reason}')
+def cancelation_handler(event_args):
+    print(f'Canceled: {event_args.reason}')
     if event_args.reason == azuresystem.CancellationReason.Error:
         print(f"Error: {event_args.error_details}")
 
 # Thread & Pynput listener Combo for mic_recorder:
 mic_thread_running = False
+final_text = ''
 
 def mic_recorder():
     # Setup
-    talking_stick = "api_key"
-    service_region = 'region'
+    global final_text
+    talking_stick = os.getenv('API_KEY')
+    service_region = os.getenv('REGION')
     audio_config = AudioConfig(use_default_microphone=True)
     speech_config = azuresystem.SpeechConfig(subscription=talking_stick, region=service_region)
     speech_config.speech_recognition_language = 'en-US'
@@ -51,6 +54,7 @@ def mic_recorder():
     finally:
         recognizer.stop_continuous_recognition()
         print('Mic Drop!!!')
+        print(f'{final_text}')
 
 
 
@@ -66,28 +70,9 @@ def spacebar_checker(key):
             return False    
 
 # Thread
-# mic_input = thread.Thread(target=mic_recorder)   
+mic_input = thread.Thread(target=mic_recorder)   
 
 # Call
-# print('Press Spacebar')
-# with keyboard.Listener(on_press=spacebar_checker) as listener:
-#    listener.join()
-
-final_text = ''
-talking_stick = "api_key"
-service_region = 'region'
-audio_config = AudioConfig(use_default_microphone=True)
-speech_config = azuresystem.SpeechConfig(subscription=talking_stick, region=service_region)
-speech_config.speech_recognition_language = 'en-US'
-recognizer = azuresystem.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-
-recognizer.recognized = recognized_handler
-recognizer.recognizing = recognizing_handler
-recognizer.canceled = cancelation_handler
-
-print("You're Live!!!!")
-recognizer.start_continuous_recognition
-value = input('Press enter: ')
-print('Mic Drop!!!!') 
-recognizer.start_continuous_recognition 
-print(f'{final_text}')
+print('Press Spacebar')
+with keyboard.Listener(on_press=spacebar_checker) as listener:
+    listener.join()
--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 Azure Text to Speech Program:
 
 Currently this system utilizes the following libraries:
-- azure.cognitiveservices.speech
-    - AudioConfig
-    - ResultReason
-- thread
-- pynput
-    - keyboard
-- time
+azure.cognitiveservices.speech
+azure.cognitiveservices.speech, AudioConfig
+thread
+pynput, keyboard
+time
+os
 
-To set up correctly, you will need to export your api_key and region in the terminal. Looks like this;
+To set up correctly, you will need to export your api_key and region as env variavles in the relevant output terminal. Looks like this;
 export api_key='your_actual_key'
 export region='your_actual_region'
+For this program I used the os library to call the env variables, however this isn't essential.
 
-Still work that needs to be done. Lots of debugging, but we're getting there!
+The program is designed to listen for the spacebar as a prompt to both start and end the STT process. You can change this in the spacebar_checker function by utilizing relevant pynput library code.
 
+Using the recognizing event handler, it will also constantly update you on any word it hears. If you'd like to disable this I recommend deleting the recognizing_handler caller from the mic_recorder func. It is unnessential and will not break anything upon removal (or at least it shouldn't).
